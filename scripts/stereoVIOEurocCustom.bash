cb8627c3d4fadf9cf93ecb4d78b6f9eceba6023b
diplodocuslongus <hinoki.kuaimu@gmail.com>
2025-05-21 20:17:33 +0800

    change verbose level: instructive!

--- scripts/stereoVIOEurocCustom.bash
+++ scripts/stereoVIOEurocCustom.bash
@@ -103,7 +103,7 @@ $BUILD_PATH/stereoVIOEurocCustom \
   --logtostderr=1 \
   --colorlogtostderr=1 \
   --log_prefix=1 \
-  --v=0 \
+  --v=100 \
   --vmodule=Pipeline*=00 \
   --log_output="$LOG_OUTPUT" \
   --log_euroc_gt_data=0 \
